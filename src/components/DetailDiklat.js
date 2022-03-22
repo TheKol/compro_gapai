70d7bc3eb7be3938cc45a2e447e5005373bfc654
Raden Indra Akbar Saputra <afyrafli00@gmail.com>
2022-03-22 11:20:39 +0700

    detail page update style

--- src/components/DetailDiklat.js
+++ src/components/DetailDiklat.js
@@ -3,7 +3,7 @@ import { Splide, SplideSlide } from '@splidejs/react-splide';
 
 const SATPAM = [
   {
-    imageSrc: 'images/sma-angkasa-husaein.jpeg',
+    imageSrc: 'images/kegiatan1.jpeg',
     imageAlt: 'SMA Angkasa',
   },
   {
@@ -42,26 +42,24 @@ const SATPAM = [
 
 function DetailDiklat() {
   return (
-    <div className='px-8 py-28'>
-      <h2 className='text-center text-5xl font-bold mb-28'>
-        PENDIDIKAN DAN PELATIHAN
-        <br />
-        GADA PRATAMA
-      </h2>
-      <div className='flex justify-evenly'>
-        <div className='w-1/3'>
-          <h2 className='text-3xl font-bold mb-2'>Gada Pratama</h2>
-          <p className='mb-2 text-xl text-justify'>
+    <div className='px-8 lg:py-28 py-10'>
+      <div className='flex flex-col-reverse lg:flex-row justify-evenly'>
+        <div className='flex flex-col lg:flex-grow w-full lg:max-w-2xl mb-7 lg:mr-10 xl:mr-24'>
+          <h2 className='text-3xl font-bold mb-2'>
+            PENDIDIKAN DAN PELATIHAN GADA PRATAMA
+          </h2>
+          <p className='text-xl text-justify'>
             Training security dan{' '}
             <span className='font-bold'>pelatihan satpam Gada Pratama</span>{' '}
             adalah pendidikan dan pelatihan dasar wajib bagi calon anggota
             satpam. Dalam pelatihan ini peserta akan belajar untuk mengetahui
             bagaimana wewenang serta kewajiban seorang satpam.
           </p>
-          <div>
+
+          <div className='p-6 mt-6 rounded-3xl border-2 h-fit shadow-lg'>
             <div>
-              <p className='font-bold text-xl mt-5'>Fasilitas Training :</p>
-              <ul className='list-disc text-xl ml-5 mt-2'>
+              <p className='font-bold text-base'>Fasilitas Training :</p>
+              <ul className='list-disc text-sm font-semibold ml-5'>
                 <li>Baju Kaos Satpam</li>
                 <li>Topi Satpam</li>
                 <li>Pin Gada Pratama</li>
@@ -70,46 +68,46 @@ function DetailDiklat() {
                 <li>Snack dan Makan Siang</li>
               </ul>
             </div>
+            <div>
+              <p className='font-bold text-base mt-6'>
+                Syarat Pendaftaran Training :
+              </p>
+              <ul className='list-disc ml-5 text-sm font-semibold'>
+                <li>Fotocopy KTP</li>
+                <li>Fotocopy Ijazah (Minimal SMA / Sederajat)</li>
+                <li>Fotocopy Kartu Keluarga</li>
+                <li>Surat Keterangan Sehat</li>
+                <li>Fotocopy SKCK / Rumus Sidik Jari</li>
+                <li>
+                  Foto Latar Biru Pakaian PDH Satpam 2x3, 3x4 &amp; 4x6
+                  Masing-Masing 4 Lembar
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
-        <div className='flex flex-col gap-12 w-1/3'>
-          <div className='bg-[#0070f6] text-white p-8 rounded-3xl h-fit shadow-lg shadow-[#2d89f8]'>
-            <p className='font-bold text-3xl'>Syarat Pendaftaran Training :</p>
-            <ul className='list-disc text-xl ml-5 mt-2 font-semibold'>
-              <li>Fotocopy KTP</li>
-              <li>Fotocopy Ijazah (Minimal SMA / Sederajat)</li>
-              <li>Fotocopy Kartu Keluarga</li>
-              <li>Surat Keterangan Sehat</li>
-              <li>Fotocopy SKCK / Rumus Sidik Jari</li>
-              <li>
-                Foto Latar Biru Pakaian PDH Satpam 2x3, 3x4 &amp; 4x6
-                Masing-Masing 4 Lembar
-              </li>
-            </ul>
-          </div>
-          <div className='items-center'>
-            <Splide
-              options={{
-                perPage: 4,
-                type: 'loop',
-                autoplay: true,
-                resetProgress: false,
-                arrows: false,
-                pagination: false,
-              }}
-              hasSliderWrapper
-            >
-              {SATPAM.map((mitra) => (
-                <SplideSlide key={mitra.imageSrc}>
-                  <img
-                    src={mitra.imageSrc}
-                    alt={mitra.imageAlt}
-                    className='object-cover w-32 h-32 rounded-2x1'
-                  />
-                </SplideSlide>
-              ))}
-            </Splide>
-          </div>
+        <div className='self-center xl:bg-slate-500 w-full xl:w-1/3 lg:w-lg lg:max-h-[600px] lg:h-96 md:h-[600px] sm:h-[400px] h-72 my-8 rounded-xl'>
+          <Splide
+            options={{
+              perPage: 1,
+              type: 'loop',
+              autoplay: true,
+              resetProgress: false,
+              arrows: false,
+              pagination: false,
+            }}
+            hasSliderWrapper
+          >
+            {SATPAM.map((mitra) => (
+              <SplideSlide key={mitra.imageSrc}>
+                <img
+                  src={mitra.imageSrc}
+                  alt={mitra.imageAlt}
+                  className='object-cover lg:h-96 md:h-[600px] sm:h-[400px] h-72 w-[450px] xl:w-[1300px] md:w-[1000px] sm:w-[700px] rounded-xl'
+                />
+              </SplideSlide>
+            ))}
+          </Splide>
         </div>
       </div>
     </div>
--- src/components/DetailDiklatMadya.js
+++ src/components/DetailDiklatMadya.js
@@ -3,7 +3,7 @@ import { Splide, SplideSlide } from '@splidejs/react-splide';
 
 const SATPAM = [
   {
-    imageSrc: 'images/sma-angkasa-husaein.jpeg',
+    imageSrc: 'images/kegiatan1.jpeg',
     imageAlt: 'SMA Angkasa',
   },
   {
@@ -42,78 +42,77 @@ const SATPAM = [
 
 function DetailDiklatMadya() {
   return (
-    <div className='px-8 py-28 bg-gray-200'>
-      <h2 className='text-center text-5xl font-bold mb-28'>
-        PENDIDIKAN DAN PELATIHAN
-        <br />
-        GADA MADYA
-      </h2>
-      <div className='flex flex-row-reverse justify-evenly'>
-        <div className='max-w-2xl'>
-          <h2 className='text-3xl font-bold mb-2'>Gada Madya</h2>
-          <p className='mb-5 text-xl text-justify'>
+    <div className='px-8 lg:py-28 py-10 bg-gray-200'>
+      <div className='flex flex-col-reverse lg:flex-row-reverse justify-evenly'>
+        <div className='flex flex-col lg:flex-grow w-full lg:max-w-2xl mb-7 lg:ml-10 xl:ml-24'>
+          <h2 className='text-3xl font-bold mb-2'>
+            PENDIDIKAN DAN PELATIHAN GADA MADYA
+          </h2>
+          <p className='text-xl text-justify'>
             Gada Madya adalah training yang diberikan kepada calon supervisor
             security yang bertujuan untuk memberikan kemampuan membentuk team
             security yang solid, memiliki kemampuan managerial dan leadership
             serta memenuhi kualifikasi supervisor security.
           </p>
 
-          <div>
-            <p className='font-bold text-xl mt-2'>Fasilitas Training :</p>
-            <ul className='list-disc text-xl ml-5 mt-2'>
-              <li>Baju Kaos Diklat (2 Buah)</li>
-              <li>Topi Satpam</li>
-              <li>Tas</li>
-              <li>Pin Gada Madya</li>
-              <li>Nama Bordir</li>
-              <li>Ijazah Gada Madya</li>
-              <li>KTA Gada Madya</li>
-              <li>Snack dan Makan Siang</li>
-            </ul>
+          <div className='p-6 mt-6 rounded-3xl bg-white border-2 h-fit shadow-lg'>
+            <div>
+              <p className='font-bold text-base'>Fasilitas Training :</p>
+              <ul className='list-disc text-sm font-semibold ml-5'>
+                <li>Baju Kaos Diklat (2 Buah)</li>
+                <li>Topi Satpam</li>
+                <li>Tas</li>
+                <li>Pin Gada Madya</li>
+                <li>Nama Bordir</li>
+                <li>Ijazah Gada Madya</li>
+                <li>KTA Gada Madya</li>
+                <li>Snack dan Makan Siang</li>
+              </ul>
+            </div>
+            <div>
+              <p className='font-bold text-base mt-6'>
+                Syarat Pendaftaran Training :
+              </p>
+              <ul className='list-disc ml-5 text-sm font-semibold'>
+                <li>Fotocopy KTP</li>
+                <li>Fotocopy Ijazah (Minimal SMA)</li>
+                <li>
+                  Fotocopy Ijazah Gada Pratama / Surat Keterangan dari Tempat
+                  Kerja
+                </li>
+                <li>Fotocopy Kartu Keluarga</li>
+                <li>Surat Keterangan Sehat</li>
+                <li>Fotocopy SKCK / Rumus Sidik Jari</li>
+                <li>
+                  Foto Latar Kuning Pakaian PDH Satpam 2x3, 3x4 &amp; 4x6
+                  Masing-Masing 4 Lembar
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
-        <div className='flex flex-col gap-12 w-1/3'>
-          <div className='bg-[#0070f6] text-white p-8 rounded-3xl h-fit shadow-lg shadow-[#2d89f8]'>
-            <p className='font-bold text-3xl'>Syarat Pendaftaran Training :</p>
-            <ul className='list-disc text-xl ml-5 mt-2 font-semibold'>
-              <li>Fotocopy KTP</li>
-              <li>Fotocopy Ijazah (Minimal SMA)</li>
-              <li>
-                Fotocopy Ijazah Gada Pratama / Surat Keterangan dari Tempat
-                Kerja
-              </li>
-              <li>Fotocopy Kartu Keluarga</li>
-              <li>Surat Keterangan Sehat</li>
-              <li>Fotocopy SKCK / Rumus Sidik Jari</li>
-              <li>
-                Foto Latar Kuning Pakaian PDH Satpam 2x3, 3x4 &amp; 4x6
-                Masing-Masing 4 Lembar
-              </li>
-            </ul>
-          </div>
-          <div className='items-center'>
-            <Splide
-              options={{
-                perPage: 4,
-                type: 'loop',
-                autoplay: true,
-                resetProgress: false,
-                arrows: false,
-                pagination: false,
-              }}
-              hasSliderWrapper
-            >
-              {SATPAM.map((mitra) => (
-                <SplideSlide key={mitra.imageSrc}>
-                  <img
-                    src={mitra.imageSrc}
-                    alt={mitra.imageAlt}
-                    className='object-cover w-32 h-32 rounded-2x1'
-                  />
-                </SplideSlide>
-              ))}
-            </Splide>
-          </div>
+        <div className='self-center xl:bg-slate-500 w-full xl:w-1/3 lg:w-lg lg:max-h-[600px] lg:h-96 md:h-[600px] sm:h-[400px] h-72 my-8 rounded-xl'>
+          <Splide
+            options={{
+              perPage: 1,
+              type: 'loop',
+              autoplay: true,
+              resetProgress: false,
+              arrows: false,
+              pagination: false,
+            }}
+            hasSliderWrapper
+          >
+            {SATPAM.map((mitra) => (
+              <SplideSlide key={mitra.imageSrc}>
+                <img
+                  src={mitra.imageSrc}
+                  alt={mitra.imageAlt}
+                  className='object-cover lg:h-96 md:h-[600px] sm:h-[400px] h-72 w-[450px] xl:w-[1300px] md:w-[1000px] sm:w-[700px] rounded-xl'
+                />
+              </SplideSlide>
+            ))}
+          </Splide>
         </div>
       </div>
     </div>
