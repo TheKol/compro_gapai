e484c3a9a2c0eb501292d955a5055e4051a6e532
Raden Indra Akbar Saputra <afyrafli00@gmail.com>
2022-03-23 17:09:03 +0700

    add gambar barang

--- src/components/MotoCapai.js
+++ src/components/MotoCapai.js
@@ -34,10 +34,12 @@ const MotoCapai = () => {
   return (
     <div className='flex flex-col items-center p-16 gap-8 bg-[#f8f9fa]'>
       <div className='flex flex-col items-center gap-4'>
-        <h2 className='text-[#223f96] font-semibold text-5xl'>MOTO CAPAI</h2>
+        <h2 className='text-[#223f96] font-semibold sm:text-5xl text-4xl'>
+          MOTO CAPAI
+        </h2>
         <div className='border-b-2 border-[#223f96] w-64'></div>
       </div>
-      <p className='text-center text-xl font-medium'>
+      <p className='text-center sm:text-xl text-lg font-medium'>
         Menjadi Sebuah Yayasan Pendidikan yang bermutu dengan standar nasional,
         <br /> untuk membentuk insan yang berkarakter, dalam membangun negara
         bangsa dan agama.
@@ -45,7 +47,7 @@ const MotoCapai = () => {
         sistem pembinaan, dalam bentuk insan yang berkarakter, dalam membangun
         negara bangsa dan agama.
       </p>
-      <div className='flex flex-row gap-4'>
+      <div className='grid grid-flow-row sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-4 '>
         {MOTO_CAPAI.map((moto) => (
           <MotoCard key={moto.title} data={moto} />
         ))}
