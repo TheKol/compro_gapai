bc2a5478bcb2e69f0320c91d9c9ed584d3bb471f
Raden Indra Akbar Saputra <afyrafli00@gmail.com>
2022-03-24 16:11:40 +0700

    responsif supplier

--- src/components/ListSupply.js
+++ src/components/ListSupply.js
@@ -621,7 +621,7 @@ const ListSupply = () => {
   return (
     <div className='flex flex-col items-center'>
       <h3 className='mb-8 text-2xl font-bold'>Items</h3>
-      <div className='grid grid-cols-5 gap-4 mb-16'>
+      <div className='grid 2xl:grid-cols-5 xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4 mb-16'>
         {SUPPLY_ITEMS.map((item) => (
           <ItemCard />
         ))}
--- src/pages/PengadaanBarang.js
+++ src/pages/PengadaanBarang.js
@@ -13,10 +13,10 @@ const PengadaanBarang = () => {
       >
         <div className='w-screen h-44 bg-slate-400'></div>
         <div className='flex flex-col items-center'>
-          <h2 className='text-4xl font-extrabold my-7'>
+          <h2 className='lg:text-4xl md:text-3xl sm:text-2xl text-lg text-center font-extrabold my-7'>
             Jelajahi Katalog Supply Kebutuhan Sekolah
           </h2>
-          <p className='w-[760px] p-5 text-center mb-4'>
+          <p className='lg:w-[760px] w-full p-5 text-center mb-4'>
             PT. Garda Media Putra Indonesia menyediakan jasa yang tepat untuk
             kebutuhan Sekolah Anda. Kami menyediakan produk kebutuhan Sekolah
             dengan harga bersaing.
